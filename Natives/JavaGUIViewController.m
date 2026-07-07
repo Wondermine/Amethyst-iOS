d4b31bbb246af6f3e5a698a458cc636e9c5dce2a
Taha Ali <TahaAbdulgader0@gmail.com>
2026-07-07 10:41:07 +0200

    Add files via upload
    Signed-off-by: Taha Ali <TahaAbdulgader0@gmail.com>

--- Natives/JavaGUIViewController.m
+++ Natives/JavaGUIViewController.m
@@ -259,6 +259,8 @@ void AWTInputBridge_sendKey(int keycode) {
     self.inputTextField.sendChar = ^(jchar keychar){
         AWTInputBridge_sendChar(keychar);
     };
+
+    __weak typeof(self) weakSelf = self;
     self.inputTextField.sendKey = ^(int key, int scancode, int action, int mods) {
         if (action == 0) return;
         switch (key) {
@@ -266,7 +268,15 @@ void AWTInputBridge_sendKey(int keycode) {
                 AWTInputBridge_sendKey('\b'); // VK_BACK_SPACE
                 break;
             case GLFW_KEY_ENTER:
-                AWTInputBridge_sendKey('\n'); // VK_ENTER;
+                AWTInputBridge_sendKey('\n'); // VK_ENTER
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    JavaGUIViewController *strongSelf = weakSelf;
+                    if (!strongSelf) return;
+
+                    strongSelf.inputTextField.text = @"";
+                    [strongSelf.inputTextField resignFirstResponder];
+                    [strongSelf.view endEditing:YES];
+                });
                 break;
             case GLFW_KEY_DPAD_LEFT:
                 AWTInputBridge_sendKey(0xE2); // VK_KP_LEFT;
@@ -494,11 +504,12 @@ dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     self.inputTextField.sendKey(GLFW_KEY_ENTER, 0, 1, 0);
-    self.inputTextField.sendKey(GLFW_KEY_ENTER, 0, 0, 0);
 
     textField.text = @"";
     [textField resignFirstResponder];
-    return YES;
+    [self.view endEditing:YES];
+
+    return NO;
 }
 
 
